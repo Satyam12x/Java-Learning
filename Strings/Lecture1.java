2f51a9e243f33aaa7e903f4910142de36a9b4cc5
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-15 00:00:51 +0530

    Introduction to strings

---
+++ Strings/Lecture1.java
@@ -0,0 +1,31 @@
+// //Strings : In Java, a String is an object that represents a sequence of characters. It is one of the most fundamental and widely used classes in the java.lang package. 
+
+// //Declaration and initializationof String:
+
+// String name = "Satyam"; // Way - 1
+
+// String name = new String("Satyam"); //Way - 2
+
+
+// //In java, Strings are immutable means it cannot be changed. Once a String object is created, its value cannot be changed. Any operation that appears to modify a String (like concatenation or substring extraction) actually results in the creation of a new String object.
+
+// //Taking a string as input:
+// /*
+// - We can take string as input from the user with the help of Scanner class.
+// - using the inbuilt method .nextLine() and .next().
+// */
+// String name = scanner.next(); //=> used for taking only first word as input.
+// String name = scanner.nextLine(); //=> used for taking more than one words or a line as input.
+
+//Code Snippet of String declaration and input and output
+
+import java.util.*;
+
+public class Lecture1 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        String firstName; 
+        name = "Satyam";
+        
+    } 
+}
