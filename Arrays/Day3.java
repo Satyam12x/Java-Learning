08ff8e44a5f961d79b8125be65c18d13bd355322
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-14 15:39:57 +0530

    added solution of previous questions

--- Arrays/Day3.java
+++ Arrays/Day3.java
@@ -35,7 +35,7 @@ public class Solution1{
 
 //Ques 2:
 import java.util.*;
-public class Solution1{
+public class Solution2{
     public static void main(String args[]){
         Scanner s = new Scanner(System.in);
         int arr[] = new int[5];
@@ -59,7 +59,7 @@ public class Solution1{
 
 //Ques 3:
 import java.util.*;
-public class Solution1{
+public class Solution3{
     public static void main(String args[]){
         int arr[] = {
             11, 31, 21, 8, 55, 43
---
+++ Arrays/Day5.java
@@ -0,0 +1,15 @@
+//Practice questions on 2-D arrays
+
+/*
+Ques 1. WAP to take a 2-d array input from the user and then print that array in a matrix manner.
+Ques 2. WAP to perform arithmatic operations on 2 matrices.
+Ques 3. WAP to transpose a matrix.
+
+#HomeWork:
+Ques 1. WAP to search for an element in an array.
+*/
+
+//Solution 1:
+public class Solution1{
+    
+}
