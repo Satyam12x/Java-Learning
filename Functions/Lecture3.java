c58509818495a7ff5ba6aaf502c207b65e7dcceb
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-16 13:22:23 +0530

    Update Lecture3.java

--- Functions/Lecture3.java
+++ Functions/Lecture3.java
@@ -1,15 +1,18 @@
+import java.util.*;
 public class Factorial {
-    public static int factorial(int n) {
-        // Base case: if n is 0 or 1, return 1
-        if (n == 0 || n == 1) {
+    public static int factorial(int n){
+        int res = 1;
+        if(n==1){
             return 1;
         }
-        // Recursive case: n * factorial of (n-1)
-        return n * factorial(n - 1);
+        return n * factorial(n-1);
     }
 
     public static void main(String[] args) {
-        int result = factorial(5);
-        System.out.println("Factorial of 5 is: " + result); // Output: Factorial of 5 is: 120
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = s.nextInt();
+        int result = factorial(n);
+        System.out.println("Factorial is: " + result); // Output: Factorial of 5 is: 120
     }
 }
