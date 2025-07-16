33b52c0d241550b762082843ed861811e542d12f
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-16 09:56:05 +0530

    Starting functions or methods

---
+++ Functions/Lecture1.java
@@ -0,0 +1,13 @@
+/*
+Functions: Fucntion is a block of code which performs specific task.
+- In Java, functions are called methods. A method is a block of code designed to perform a specific task and is associated with a class or an object.
+* Methods help in:
+- Code Reusability: Write once, use multiple times.
+- Modularity: Break down complex programs into smaller, manageable pieces.
+- Abstraction: Hide implementation details and expose only functionality.
+- Methods can return a value or perform an action without returning anything (void).
+*/
+
+/*
+
+*/
