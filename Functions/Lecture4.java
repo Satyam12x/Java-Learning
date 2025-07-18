698d6e9af3fca82b7a6299aa9a419e30dc48357f
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-18 08:35:28 +0530

    Practice Question on Recursion

--- Functions/Lecture4.java
+++ Functions/Lecture4.java
@@ -31,3 +31,20 @@ public class Lecture4 {
         printNumbers(n); //n=1
     }
 }
+
+//Ques 3: Print the sum of first n natural numbers:
+public class Lecture4{
+    public static void printSum(int sum, int i, int n){
+        if(i==n){
+            sum += i;
+            System.out.println(sum);
+            return;
+        }
+        
+        sum += i; //sum = 1
+        printSum(sum, i+1, n);
+    }
+    public static void main(String[] args){
+        printSum(0, 1, 10); // sum = 0; i = 1; n = 10
+    }
+}
