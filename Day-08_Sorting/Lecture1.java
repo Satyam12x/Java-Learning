f4e52bce66f802097982db3e8c659d9b5ebd8d43
Satyam Pandey <ofcsatyam007@gmail.com>
2025-07-23 17:48:29 +0530

    Introduction to sorting

---
+++ Day-08_Sorting/Lecture1.java
@@ -0,0 +1,28 @@
+/*
+# What is Sorting?
+- Sorting is the process of arranging data in a particular format (usually ascending or descending).
+
+Used in:
+
+- Searching algorithms
+
+- Data analysis
+
+- Displaying data in ordered form
+
+- Optimizing database queries
+
+#Types of Sorting
+- Internal Sorting: All data is loaded into memory (e.g., Arrays).
+
+- External Sorting: Used for large data that doesn’t fit in memory (e.g., files).
+
+#Criteria:
+- Time Complexity
+
+- Space Complexity
+
+- Stability (does it preserve the order of equal elements?)
+
+- Adaptability (does it perform better on partially sorted data?)
+*/
