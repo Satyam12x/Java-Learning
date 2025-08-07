581af30c639b90fabfc320fb1a0c0283c3a3725c
Satyam Pandey <ofcsatyam007@gmail.com>
2025-08-07 23:09:53 +0530

    Update Lecture2.java

--- OOPs/Lecture2.java
+++ OOPs/Lecture2.java
@@ -1,1 +1,8 @@
 //In this lecture, we will learn about inheritance.
+
+/*
+# What is Inheritance?
+- Inheritance is one of the core concepts of Object-Oriented Programming (OOP). It allows a class (called the child or subclass) to acquire the properties and behaviors (fields and methods) of another class (called the parent or superclass).
+
+
+*/
